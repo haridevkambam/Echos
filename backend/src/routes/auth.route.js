7a3e54df553655339161d46b85e82448c741805c
haridevkambam <haridevkambam@gmail.com>
2025-01-06 13:42:33 +0530

    fixed bug, add emit to receiver for new message

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
-import { protectRoute} from "../middlewares/auth.middleware.js";
+import { protectRoute} from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.post("/signup", signup);
